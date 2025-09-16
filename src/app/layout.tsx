7aa48d89ba2be6b1355694f18fab46669e050609
Sheng Hung Tsai <kuma@Shengs-MacBook-Air.local>
2025-09-15 18:58:00 -0600

    removed unused variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ApolloProvider } from "@apollo/client/react";
@@ -18,11 +17,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-export const metadata: Metadata = {
-  title: "Sheng Hung Tsai",
-  description: "Since 1990",
-};
 
 const TempWrapper = ({
   children,
